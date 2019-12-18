f95c98738d8d9624009b41d7009b30ac52896e19
Luis Borello <luis_borello@hotmail.com>
2019-12-18 11:30:19 -0300

    rutas y modulos
    rutas y modulos

--- adminpro/src/app/app.module.ts
+++ adminpro/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// Rutas
-import { APP_ROUTES } from './app.routes';
-
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
+// Rutas
+import { APP_ROUTES } from './app.routes';
 // Modulos
 import { PagesModule } from './pages/pages.module';
 @NgModule({
