b70cdd9a294c73bab03b939f5a0dd992a5744efc
Luis Borello <luis_borello@hotmail.com>
2020-01-21 14:34:30 -0300

    terminado
    terminado

--- UpTaskNodeJS/index.js
+++ UpTaskNodeJS/index.js
@@ -1,82 +1,95 @@
 const express = require('express');
 const routes = require('./routes');
-// const path = require('path');
+const path = require('path');
 // const bodyParser = require('body-parser');
-// const expressValidator = require('express-validator');
-// const flash = require('connect-flash');
-// const session = require('express-session');
-// const cookieParser = require('cookie-parser');
-// const passport = require('./config/passport');
+const expressValidator = require("express-validator");
+// const expressValidator = require('express-validator/check');
+const flash = require('connect-flash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const passport = require('./config/passport');
+// helpers con algunas funciones
+const helpers = require('./helpers');
+// Crear la conexión a la BD
+const db = require('./config/db');
+//extraer valores de variables 
+require('dotenv').config({ path: 'variables.env' });
 
-// // helpers con algunas funciones
-// const helpers = require('./helpers');
+// Importar el modelo
+require('./models/Proyectos');
+require('./models/Tareas');
+require('./models/CONTENEDOR');
+require('./models/Usuarios');
 
-// // Crear la conexión a la BD
-// const db = require('./config/db2');
-// // const db = require('./config/db1');
-
-// // Importar el modelo
-// require('./models/Proyectos');
-// require('./models/Tareas');
-// require('./models/Usuarios');
-
-// db.sync()
-//     .then(() => console.log('Conectado al Servidor'))
-//     .catch(error => console.log(error));
+db.sync(
+        // { force: true }
+        // sincronizar la base de datos con los modelos OJO BORRA TODO
+    )
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 // crear una app de express
 const app = express();
-
 // // Donde cargar los archivos estaticos
-// app.use(express.static('public'));
-
+app.use(express.static('public'));
 // // Habilitar Pug
-// app.set('view engine', 'pug');
+app.set('view engine', 'pug');
+// // Añadir la carpeta de las vistas
 
-// // habilitar bodyParser para leer datos del formulario
-
-// app.use(bodyParser.json());
+// habilitar bodyParser para leer datos del formulario
+app.use(express.urlencoded({ extended: false }));
 // app.use(bodyParser.urlencoded({ extended: false }));
 
-// // Agregamos express validator a toda la aplicación
+// Agregamos express validator a toda la aplicación
 // app.use(expressValidator());
 
+app.set('views', path.join(__dirname, './views'));
+// agregar flash messages
+app.use(flash());
+// sessiones nos permiten navegar entre distintas paginas sin volvernos a autenticar
+app.use(session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cookieParser());
+// Pasar var dump a la aplicación
+app.use((req, res, next) => {
+    console.log(req.user);
+    res.locals.year = 2019;
+    res.locals.vardump = helpers.vardump;
+    res.locals.mensajes = req.flash();
+    res.locals.usuario = {...req.user } || null;
+    next();
+});
+
+app.use('/', routes());
 
 
-// // Añadir la carpeta de las vistas
-// app.set('views', path.join(__dirname, './views'));
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+// // luis
+app.listen(port, host, () => {
+    console.log('El servidor esta funcionando');
+});
 
 
 
+// parse application/json
+// app.use(bodyParser.json());
+
+// console.log(__dirname);
 // app.use(cookieParser());
-
 // // sessiones nos permiten navegar entre distintas paginas sin volvernos a autenticar
 // app.use(session({
 //     secret: "keyboard cat",
 //     resave: false,
 //     saveUninitialized: false
 // }));
-
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// // agregar flash messages
-// app.use(flash());
-
-// // Pasar var dump a la aplicación
-// app.use((req, res, next) => {
-//     res.locals.vardump = helpers.vardump;
-//     res.locals.mensajes = req.flash();
-//     res.locals.usuario = {...req.user } || null;
-//     next();
-// });
-
-// app.use('/', (req, res) => {
-//     res.send('hola');
-// });
-
-app.use('/', routes());
-
-// // luis
-app.listen(3000);
